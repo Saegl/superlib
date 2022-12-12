d352b54c2f9d82f19466ae1381949bbd00fa284a
Saegl <saegl@protonmail.com>
2022-12-12 20:12:47 +0600

    Init FastAPI

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="SuperLib",
+    description="Websystem for sharing both physical and electronic books",
+    version="0.1.0",
+)
---
+++ app/__main__.py
@@ -0,0 +1,3 @@
+import uvicorn
+
+uvicorn.run("app:app", port=1234, reload=True)
