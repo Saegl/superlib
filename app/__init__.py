76f234dd6aca1a45d9ad0de35ba07e38b9d17b65
Saegl <saegl@protonmail.com>
2022-12-12 20:41:18 +0600

    Init tortoise orm

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from app.models import User
+
+################### Settings
+APP_URL = "postgres://demo:demo@localhost:5432/superlib"
 
 app = FastAPI(
     title="SuperLib",
     description="Websystem for sharing both physical and electronic books",
     version="0.1.0",
 )
+register_tortoise(
+    app,
+    db_url=APP_URL,
+    modules={"models": ["app.models"]},
+    generate_schemas=True,
+)
+
+
+################### Routers
+@app.post("/register")
+async def register():
+    user = await User.create(
+        name="Alisher",
+        surname="Zhubanyshev",
+        email="zhubanysh.alisher@gmail.com",
+        password_hash="1234",
+    )
+    return {"message": user.name}
+
+
+@app.post("/signin")
+async def signin():
+    return {"message": "WIP"}
---
+++ app/models.py
@@ -0,0 +1,11 @@
+from tortoise import fields, models
+
+
+class User(models.Model):
+    id = fields.UUIDField(pk=True)
+
+    name = fields.CharField(max_length=255)
+    surname = fields.CharField(max_length=255)
+
+    email = fields.CharField(max_length=255)
+    password_hash = fields.CharField(max_length=255)
