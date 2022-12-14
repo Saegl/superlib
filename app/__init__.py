9b4070c31113f52451ca476f2164334f06e5fc93
Saegl <saegl@protonmail.com>
2022-12-15 01:08:34 +0600

    Add fake books

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Request, Form, status, Cookie, Depends
+import math
+
+from fastapi import FastAPI, Request, Form, status, Cookie, Depends, Query
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -7,7 +9,7 @@ from tortoise.exceptions import DoesNotExist, OperationalError
 from tortoise.contrib.fastapi import register_tortoise
 from passlib.context import CryptContext
 
-from app.models import User
+from app.models import User, Author, Book, Publisher
 
 ################### Settings
 APP_URL = "postgres://demo:demo@localhost:5432/superlib"
@@ -40,8 +42,28 @@ async def get_user(userid: str = Cookie(default="-1")) -> User | None:
 
 ################### Routers
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request, user: User = Depends(get_user)):
-    return templates.TemplateResponse("index.html", {"request": request, "user": user})
+async def index(
+    request: Request, user: User = Depends(get_user), page: int = Query(default=1)
+):
+    offset = (page - 1) * 16
+    books = await Book.all().offset(offset).limit(16).order_by("-created_at")
+    books_count = await Book.all().count()
+    pages_count = math.ceil(books_count / 16)
+
+    first_page = max(page - 5, 1)
+    last_page = min(first_page + 10, pages_count) + 1
+    pages = list(range(first_page, last_page))
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "user": user,
+            "books": books,
+            "current_page": page,
+            "pages": pages,
+        },
+    )
 
 
 @app.get("/about", response_class=HTMLResponse)
@@ -56,11 +78,19 @@ async def contact(request: Request, user: User = Depends(get_user)):
     )
 
 
-@app.get("/book/{id}", response_class=HTMLResponse)
-async def book(request: Request, user: User = Depends(get_user)):
+@app.get("/book/{isbn}", response_class=HTMLResponse)
+async def book(request: Request, isbn: str, user: User = Depends(get_user)):
+    book = await Book.get(isbn=isbn)
+    book.views += 1
+    # book.created_at = book.created_at
+    await book.save()
     return templates.TemplateResponse(
         "book-detail.html",
-        {"request": request, "title": "The Computer Science book", "user": user},
+        {
+            "request": request,
+            "user": user,
+            "book": book,
+        },
     )
 
 
@@ -119,6 +149,42 @@ async def logout():
     resp = RedirectResponse("/", status_code=status.HTTP_302_FOUND)
     resp.set_cookie("userid", "-1")
     return resp
+
+
+@app.post("/gendata")
+async def gendata():
+    from faker import Faker
+    from random import randint
+
+    fake = Faker()
+
+    for _ in range(100):
+        name, surname = fake.name().split(maxsplit=1)
+        author = await Author.create(
+            name=name,
+            surname=surname,
+            bio=fake.text(),
+            email=fake.email(),
+            password_hash="1234",
+        )
+
+        publisher = await Publisher.create(
+            name=fake.company(),
+        )
+
+        for _ in range(10):
+            await Book.create(
+                isbn=fake.isbn13(),
+                author=author,
+                title=fake.sentence(),
+                image_url=f"https://placekitten.com/{randint(400, 600)}/{randint(600, 800)}",
+                publisher=publisher,
+                year=fake.year(),
+                pages=randint(50, 1000),
+                views=0,
+            )
+
+    return {"status": "Success"}
 
 
 app.mount(
--- app/models.py
+++ app/models.py
@@ -10,3 +10,31 @@ class User(models.Model):
 
     email = fields.CharField(max_length=255)
     password_hash = fields.CharField(max_length=255)
+
+
+class Author(models.Model):
+    id = fields.IntField(pk=True)
+
+    name = fields.CharField(max_length=255)
+    surname = fields.CharField(max_length=255)
+    bio = fields.CharField(max_length=255)
+
+
+class Publisher(models.Model):
+    id = fields.IntField(pk=True)
+
+    name = fields.CharField(max_length=255)
+
+
+class Book(models.Model):
+    isbn = fields.CharField(pk=True, max_length=255)
+
+    author = fields.ForeignKeyField("models.Author")
+    title = fields.CharField(max_length=255)
+    image_url = fields.CharField(max_length=255)
+    publisher = fields.ForeignKeyField("models.Publisher")
+    year = fields.IntField()
+    pages = fields.IntField()
+
+    created_at = fields.DatetimeField(auto_now_add=True)
+    views = fields.IntField()
